ca106827c8a300d4ca78d9ec1fedd0de543e6af7
Jan Pecha <janpecha@email.cz>
2012-11-15 09:10:36 +0100

    added libs/Nette/Object
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ libs/Nette/loader.php
@@ -0,0 +1,20 @@
+<?php
+	/** Heymaster Nette Loader
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-11-15-1
+	 */
+	
+	require __DIR__ . '/ObjectMixin.php';
+	require __DIR__ . '/Object.php';
+	
+	require __DIR__ . '/Reflection/IAnnotation.php';
+	require __DIR__ . '/Reflection/Annotation.php';
+	require __DIR__ . '/Reflection/AnnotationsParser.php';
+	require __DIR__ . '/Reflection/ClassType.php';
+	require __DIR__ . '/Reflection/Extension.php';
+	require __DIR__ . '/Reflection/GlobalFunction.php';
+	require __DIR__ . '/Reflection/Method.php';
+	require __DIR__ . '/Reflection/Parameter.php';
+	require __DIR__ . '/Reflection/Property.php';
+	
