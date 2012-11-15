99573edc06686e3d5b3581430885a387b97ca408
Jan Pecha <janpecha@email.cz>
2012-11-15 09:10:51 +0100

    added Heymaster\Cli\Cli
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ src/Cli/Cli.php
@@ -0,0 +1,85 @@
+<?php
+	/** Cli
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-11-15-1
+	 */
+	
+	namespace Heymaster\Cli;
+	
+	class Cli extends \Cz\Cli
+	{
+		/**
+		 * @param	string[]	of arguments
+		 * @return	array|FALSE
+		 */
+		public static function parseParams(array $argv)
+		{
+			$params = array();
+			$lastName = NULL;
+			
+			if(isset($argv[1]))		// count($argv) > 1
+			{
+				// remove argv[0]
+				array_shift($argv);
+				
+				// parsing
+				foreach($argv as $argument)
+				{
+					if($argument{0} === '-')
+					{
+						$name = trim($argument, '-');
+						
+						if($name !== '')
+						{
+							$lastName = $name;
+					
+							if(!isset($params[$name]))
+							{
+								$params[$name] = TRUE;
+							}
+						}
+						else
+						{
+							$lastName = NULL;
+						}
+					}
+					elseif($lastName === NULL)
+					{
+						//throw new \Exception("Bad argument '$argument'");
+						$params[] = $argument;
+					}
+					else
+					{
+						if($params[$lastName] === TRUE)
+						{
+							$params[$lastName] = $argument;
+						}
+						else	// string || array
+						{
+							if(is_string($params[$lastName]))
+							{
+								$newParams = array(
+									$params[$lastName],
+									$argument,
+								);
+							
+								$params[$lastName] = $newParams;
+							}
+							else
+							{
+								$params[$lastName][] = $argument;
+							}
+						
+							$lastName = NULL;
+						}
+					}
+				}
+				
+				return $params;
+			}
+			
+			return FALSE;
+		}
+	}
+
