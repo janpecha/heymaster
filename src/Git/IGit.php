94d6a96f5d2988baf26bf6686c10d3c5c33596a8
Jan Pecha <janpecha@email.cz>
2012-11-19 15:09:20 +0100

    Git\IGit: added methods tag, merge, branchCreate, branchRemove, checkout
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- src/Git/IGit.php
+++ src/Git/IGit.php
@@ -2,12 +2,17 @@
 	/** IGit interface
 	 * 
 	 * @author		Jan Pecha, <janpecha@email.cz>
-	 * @version		2012-11-15-1
+	 * @version		2012-11-19-1
 	 */
 	
 	namespace Heymaster\Git;
 	
 	interface IGit
 	{
+		public function tag($name);
+		public function merge($brach, $intoThis = FALSE);
+		public function branchCreate($name, $checkout = FALSE);
+		public function branchRemove($name);
+		public function checkout($name);
 	}
 
