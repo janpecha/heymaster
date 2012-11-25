0147a73fa388ed98e3c87d653a44a1912ba84df4
Jan Pecha <janpecha@email.cz>
2012-11-25 18:34:56 +0100

    added Heymaster\Config
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ src/Config.php
@@ -0,0 +1,39 @@
+<?php
+	/** Heymaster Config Class
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-11-25-1
+	 */
+	
+	namespace Heymaster;
+	
+	class Config extends \Nette\Object
+	{
+		/** @var  string */
+		public $root;
+		
+		/** @var  string|FALSE */
+		public $message;
+		
+		/** @var  bool */
+		public $output;
+		
+		
+		public function set($key, $value)
+		{
+			switch($key)
+			{
+				case 'root':
+				case 'message':
+					$this->$key = (string)$value;
+					return;
+				
+				case 'output':
+					$this->output = (bool)$value;
+					return;
+			}
+			
+			throw new \Exception("Neznama konfiguracni volba '$key'");
+		}
+	}
+
