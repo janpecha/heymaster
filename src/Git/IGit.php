57e2d72cc8fce16030cb02aca862892aa8e794aa
Jan Pecha <janpecha@email.cz>
2012-11-27 20:32:15 +0100

    added Heymaster\Git\GitException Heymaster\Git\IGit: added PhpDocs
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- src/Git/IGit.php
+++ src/Git/IGit.php
@@ -2,17 +2,48 @@
 	/** IGit interface
 	 * 
 	 * @author		Jan Pecha, <janpecha@email.cz>
-	 * @version		2012-11-19-1
+	 * @version		2012-11-27-1
 	 */
 	
 	namespace Heymaster\Git;
 	
 	interface IGit
 	{
+		/** Create a tag.
+		 * @param	string
+		 * @throws	Heymaster\Git\GitException
+		 */
 		public function tag($name);
+		
+		/**
+		 * @param	string
+		 * @throws	Heymaster\Git\GitException
+		 */
 		public function merge($brach);
+		
+		/**
+		 * @param	string
+		 * @param	bool
+		 * @throws	Heymaster\Git\GitException	
+		 */
 		public function branchCreate($name, $checkout = FALSE);
+		
+		/**
+		 * @param	string
+		 * @throws	Heymaster\Git\GitException
+		 */
 		public function branchRemove($name);
+		
+		/**
+		 * @param	string
+		 * @throws	Heymaster\Git\GitException
+		 */
 		public function checkout($name);
+	}
+	
+	
+	
+	class GitException extends \Exception
+	{
 	}
 
