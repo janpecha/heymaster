6090bf5272d6067e400208eadf9f4a93afdf94dc
Jan Pecha <janpecha@email.cz>
2012-11-27 22:57:31 +0100

    added Heymaster\DuplicateKeyException added Heymaster\InvalidException
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ src/exceptions.php
@@ -0,0 +1,19 @@
+<?php
+	/** Exceptions
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-11-27-1
+	 */
+	
+	namespace Heymaster;
+	
+	class DuplicateKeyException extends \RuntimeException
+	{
+	}
+	
+	
+	
+	class InvalidException extends \RuntimeException
+	{
+	}
+
