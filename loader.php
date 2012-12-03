7459f1e86ff13c6fd8de2308a35a97aa8bd21ba9
Jan Pecha <janpecha@email.cz>
2012-12-03 16:07:48 +0100

    added loader.php
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ loader.php
@@ -0,0 +1,36 @@
+<?php
+	/** Heymaster Loader
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-12-03-1
+	 */
+	
+	require __DIR__ . '/src/exceptions.php';
+	require __DIR__ . '/src/Heymaster.php';
+	
+	require __DIR__ . '/src/Config.php';
+	require __DIR__ . '/src/Section.php';
+	require __DIR__ . '/src/Action.php';
+	require __DIR__ . '/src/Command.php';
+	
+	require __DIR__ . '/src/Configs/FileConfig.php';
+	
+	require __DIR__ . '/src/Adapters/IAdapter.php';
+	require __DIR__ . '/src/Adapters/BaseAdapter.php';
+	require __DIR__ . '/src/Adapters/NeonAdapter.php';
+	
+	require __DIR__ . '/src/Cli/Cli.php';
+	require __DIR__ . '/src/Logger/ILogger.php';
+	require __DIR__ . '/src/Logger/DefaultLogger.php';
+	
+	require __DIR__ . '/src/Git/IGit.php';
+	require __DIR__ . '/src/Git/Git.php';
+	
+	require __DIR__ . '/src/Utils/Finder.php';
+	
+	require __DIR__ . '/src/Commands/CommandSet.php';
+	require __DIR__ . '/src/Commands/CoreCommands.php';
+#	require __DIR__ . '/src/Commands/PhpCommands.php';
+#	require __DIR__ . '/src/Commands/JsCommands.php';
+#	require __DIR__ . '/src/Commands/CssCommands.php';
+
