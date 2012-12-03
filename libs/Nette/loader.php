8a63bc1ffb404608a5c196a28352fe2fed507614
Jan Pecha <janpecha@email.cz>
2012-12-03 16:06:54 +0100

    libs/Nette: added Finder, moved some files
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- libs/Nette/loader.php
+++ libs/Nette/loader.php
@@ -5,8 +5,10 @@
 	 * @version		2012-11-15-1
 	 */
 	
-	require __DIR__ . '/ObjectMixin.php';
-	require __DIR__ . '/Object.php';
+	require __DIR__ . '/common/exceptions.php';
+	require __DIR__ . '/common/ObjectMixin.php';
+	require __DIR__ . '/common/Object.php';
+	require __DIR__ . '/common/Callback.php';
 	
 	require __DIR__ . '/Reflection/IAnnotation.php';
 	require __DIR__ . '/Reflection/Annotation.php';
@@ -18,3 +20,8 @@
 	require __DIR__ . '/Reflection/Parameter.php';
 	require __DIR__ . '/Reflection/Property.php';
 	
+	require __DIR__ . '/Iterators/Filter.php';
+	require __DIR__ . '/Iterators/RecursiveFilter.php';
+	
+	require __DIR__ . '/Utils/Finder.php';
+
