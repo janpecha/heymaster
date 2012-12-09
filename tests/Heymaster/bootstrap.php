0a7b6a1d2ce2bc8da2495fdbde9c3a63d059f86a
Jan Pecha <janpecha@email.cz>
2012-12-09 17:07:32 +0100

    tests bootstrap.php: added creating temp directory
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- tests/Heymaster/bootstrap.php
+++ tests/Heymaster/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
+/** @version	2012-12-09-1 */
 
 require __DIR__ . '/../../tools/Tester/Tester/bootstrap.php';
 require __DIR__ . '/../../libs/Nette/loader.php';
+
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/../tmp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
+Tester\Helpers::purge(TEMP_DIR);
 
 
 if (extension_loaded('xdebug')) {
