e2c7c84a8ed01e9220a868dbe4616605c7f59709
Jan Pecha <janpecha@email.cz>
2012-12-09 11:37:22 +0100

    Heymaster\Git\Git: added '-r' parameter to 'git rm' command in remove() method
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- src/Git/Git.php
+++ src/Git/Git.php
@@ -2,7 +2,7 @@
 	/** Default Git Handler
 	 * 
 	 * @author		Jan Pecha, <janpecha@email.cz>
-	 * @version		2012-12-06-3
+	 * @version		2012-12-09-1
 	 */
 	
 	namespace Heymaster\Git;
@@ -85,7 +85,7 @@
 		
 		public function remove($file)
 		{
-			$this->run("git rm", $file);
+			$this->run("git rm", $file, '-r');
 			return $this;
 		}
 		
