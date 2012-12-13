ed8eb249e24ce34551848da3892f69b0b953d6bb
Jan Pecha <janpecha@email.cz>
2012-12-13 21:42:11 +0100

    added tools/CssMinifier
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ tools/CssMinifier/CssMinifier.php
@@ -0,0 +1,57 @@
+<?php
+	/** Css Minifier - simple CSS minifier
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @license		New BSD License
+	 * @version		2012-12-13-1
+	 * @link		http://github.com/janpecha/typro
+	 */
+	
+	class CssMinifier
+	{
+		/**
+		 * @link	https://github.com/nette/build-tools/blob/master/tasks/minifyJs.php#L50
+		 * @param	string
+		 * @return	string
+		 */
+		public function minify($s)
+		{
+			$s = preg_replace('#/\*.*?\*/#s', '', $s); // remove comments
+			$s = preg_replace('#\s+#', ' ', $s); // compress space
+			$s = preg_replace('# ([^(0-9a-z.\#*-])#i', '$1', $s);
+			$s = preg_replace('#([^0-9a-z%)]) #i', '$1', $s);
+			$s = str_replace(';}', '}', $s); // remove leading semicolon
+			$s = trim($s);
+			
+			$state = 'normal';
+			$stringChar = '';
+			$len = strlen($s);
+			
+			for($i = 0; $i < $len; $i++)
+			{
+				if($state === 'normal')
+				{
+					if($s[$i] === '\'' || $s[$i] === '"')
+					{
+						$state = 'string';
+						$stringChar = $s[$i];
+					}
+					elseif($s[$i] === ' ')
+					{
+						$s[$i] = "\n";
+					}
+				}
+				elseif($state === 'string')
+				{
+					if($s[$i] === $stringChar)
+					{
+						$state = 'normal';
+						$stringChar = '';
+					}
+				}
+			}
+			
+			return $s;
+		}
+	}
+	
