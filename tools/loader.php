405274e47b467574105d48ea0bccfc7bedf8cedc
Jan Pecha <janpecha@email.cz>
2012-12-13 22:22:55 +0100

    added tools/loader.php
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ tools/loader.php
@@ -0,0 +1,10 @@
+<?php
+	/** Tools Loader
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-12-13-1
+	 */
+	
+	require __DIR__ . '/CssMinifier/CssMinifier.php';
+	require __DIR__ . '/Php-Shrink/PhpShrink.php';
+
