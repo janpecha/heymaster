c5f6bdea7bd37f74bc2989dc235b0ae615dbd500
Jan Pecha <janpecha@email.cz>
2013-02-01 13:29:07 +0100

    Tests: coverage.sh - added support for parameter "--noenter"
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 php -f ../tools/Tester/Tester/coverage-report.php -- -s "../src" -c coverage.dat -t "Heymaster" -o coverage.html
-read -p "Press [Enter] key to exit..."
+
+if [ "$1" != "--noenter" ]
+then
+    read -p "Press [Enter] key to exit..."
+fi
+
 
